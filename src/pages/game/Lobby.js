9be8868f9cd34d3cb92073df73256a982374cd22
Kris Wong <kristenccwong@gmail.com>
2025-04-04 00:31:58 +0800

    fix: animation adhering to offset

--- src/pages/game/Lobby.js
+++ src/pages/game/Lobby.js
@@ -2,85 +2,69 @@ import Phaser from "phaser";
 
 /**
  * @todo handle animations
+ * @todo automate origin setting (use the relative position of the largest sprite)
+ * @todo emit a signal on each event
  */
-const R = 2560 / 1920;
-
-const ANIMS = {
-    enter: {
-        key: "enter",
-        frames: this.anims.generateFrameNumbers("lobby_player", {
-            start: 0,
-            end: 3,
-        }),
-        frameRate: 24,
-        repeat: -1
-    },
-	idle: {
-        key: "idle",
-        frames: this.anims.generateFrameNumbers("lobby_player", {
-            start: 4,
-            end: 4,
-        }),
-        frameRate: 24,
-        repeat: -1
-    },
-	ready: {
-        key: "ready",
-        frames: this.anims.generateFrameNumbers("lobby_player", {
-            start: 5,
-            end: 16,
-        }),
-        frameRate: 24,
-        repeat: 1
-    },
-};
 
 export default class Lobby extends Phaser.Scene {
 	constructor() {
 		super("Lobby");
 	}
 
-    // hererere
-	createAnimations() {
-		this.anims.create({
-			key: "enter",
-			frames: this.anims.generateFrameNumbers("lobby_player", {
-				start: 0,
-				end: 3,
-			}),
-		});
-        this.anims.create({
-			key: "idle",
-			frames: this.anims.generateFrameNumbers("lobby_player", {
-				start: 4,
-				end: 4,
-			}),
-		});
-		this.anims.create({
-			key: "ready",
-			frames: this.anims.generateFrameNumbers("lobby_player", {
-				start: 5,
-				end: 16,
-			}),
-		});
-	}
-	playAnim(sprite, key) {
-		const anim = ANIMS[key];
-		if (!anim) return;
-		/*
-        blutbluh bluh bluh
-        */
-		//sprite.play(anim.key);
-	}
+	loadAnims() {
+		const ANIMS = {
+			lobby_player: [
+				{
+					key: "enter",
 
+					start: 0,
+					end: 3,
+					frameRate: 24,
+					repeat: -1,
+				},
+				{
+					key: "idle",
+
+					start: 4,
+					end: 4,
+
+					frameRate: 24,
+					repeat: -1,
+				},
+				{
+					key: "ready",
+					start: 5,
+					end: 16,
+					frameRate: 24,
+					repeat: 1,
+				},
+			],
+		};
+		for (let s in ANIMS) {
+			//console.log("loading anims of "+s);
+			for (let a in ANIMS[s]) {
+				//console.log(ANIMS[s][a]);
+				this.anims.create({
+					key: ANIMS[s][a].key,
+					frames: this.anims.generateFrameNames(s, {
+						prefix: s,
+						start: ANIMS[s][a].start,
+						end: ANIMS[s][a].end,
+						zeroPad: 4,
+					}),
+					repeat: ANIMS[s][a].repeat,
+				});
+			}
+		}
+	}
 	preload() {
-		this.load.image("table", "assets/lobby_table.png");
+		this.load.image("lobby_table", "assets/lobby_table.png");
 		this.load.atlas(
 			"lobby_player",
 			"assets/lobby_player.png",
 			"assets/lobby_player.json"
 		);
-		this.load.image("bg", "assets/lobby_bg.png");
+		this.load.image("lobby_bg", "assets/lobby_bg.png");
 
 		/* Get from server*/
 		this.p1Enter = false;
@@ -92,46 +76,42 @@ export default class Lobby extends Phaser.Scene {
 		const SCENE_W = this.sys.game.canvas.width;
 		const SCENE_H = this.sys.game.canvas.height;
 
-		/*** add sprites******/
-		this.bg = this.add.image(-244, -197, "bg").setOrigin(0, 0);
+		/*static sprites*/
+		this.bg = this.add.image(-244, -197, "lobby_bg").setOrigin(0, 0);
+		this.table = this.add.sprite(760, 886, "lobby_table").setOrigin(0, 0);
+		this.table.scale = 0.859;
+		this.table.setDepth(100);
 
-		// Draw debug point
-		const graphics = this.add.graphics();
-		graphics.lineStyle(2, 0xff0000);
-		graphics.strokeCircle(627, 1098, 5);
-		graphics.fillStyle(0xff0000);
-		graphics.fillCircle(627, 1098, 5);
+		/*animated sprirtes*/
+		this.p1 = this.add.sprite(0, 1098, "lobby_player").setName("p1");
+		this.p2 = this.add.sprite(0, 1098, "lobby_player").setName("p2");
 
-		// Create all animations
-		this.createAnimations();
+		// AUTOMATE THIS, use the largest anim for the stuff
+		this.p1.setOrigin(291 / this.p1.width, 800 / this.p1.height);
+		//?????
+		this.p2.setOrigin(291 / this.p2.width, 800 / this.p2.height);
 
-		//this.p1 = this.add.sprite(486,427,"p1").setOrigin(-0,-146).play("enter");
-		this.p1 = this.add.sprite(-999, 1098, "p1").setOrigin(0.5, 1).setName("p1"); // Add name for unique animation key
-		this.p2 = this.add.sprite(SCENE_W, 1098, "p2").setOrigin(0.5, 1).setName("p2"); // Add name for unique animation key
-        
-		//this.playAnim(this.p1, 'enter');
-		this.p1.play("enter");
-		this.p2.play("enter");
+		// Create dots to visualize p1's and p2's origins
+		this.p1OriginDot = this.add.circle(0, 0, 5, 0x00ff00); // Green dot for p1
+		this.p1OriginDot.setDepth(200); // Make sure it's visible above everything
+		
+		this.p2OriginDot = this.add.circle(0, 0, 5, 0xff0000); // Red dot for p2
+		this.p2OriginDot.setDepth(200); // Make sure it's visible above everything
 
-		// Initial location
+		this.loadAnims();
+
+		this.p1.play("enter", true);
+		this.p2.play("enter", true);
+
 		this.p1.setPosition(-this.p1.width, 1098);
 		this.p2.setPosition(SCENE_W + this.p2.width, 1098);
-		//this.p1.setPosition(486, 378); //test
-		//this.p2.setPosition(1610, 378);//test //How is this number??????
-
-		//this.p2.setPosition(this.p2.w + SCENE_W, 427);
-		this.table = this.add.sprite(760, 886, "table").setOrigin(0, 0);
-		this.table.scale = 0.859;
+		
 		this.p1.scale = this.p2.scale = 1;
-
 		this.p2.flipX = true;
-		this.bg.scale = 1;
+		
 
 		/******handle inputs */
-		/*
-                this.input.keyboard.on("keydown",(e)=>{
-                    this.p2.flipX = !this.p2.flipX;
-                });*/
+
 		//P1 enters at the start of the scene
 		// plays enter naim
 		//Emits a Player enters room event
@@ -145,7 +125,7 @@ export default class Lobby extends Phaser.Scene {
 			yoyo: false,
 			onComplete: () => {
 				console.log("idle");
-				this.playAnim(this.p1, "idle");
+				this.p1.play("idle");
 				this.p1Enter = true;
 			},
 		});
@@ -162,7 +142,8 @@ export default class Lobby extends Phaser.Scene {
 					repeat: 0,
 					yoyo: false,
 					onComplete: () => {
-						this.playAnim(this.p2, "idle");
+						console.log("idle");
+						this.p2.play("idle");
 						this.p2Enter = true;
 					},
 				});
@@ -174,14 +155,29 @@ export default class Lobby extends Phaser.Scene {
 			if (this.p1Enter && !this.p1Ready) {
 				console.log("Player 1 ready");
 				this.p1Ready = true;
-				this.playAnim(this.p1, "ready");
+				this.p1.play("ready");
 			} else if (this.p1Ready) {
 				console.log("Player 1 unready");
 				this.p1Ready = false;
-				this.playAnim(this.p1, "idle");
+				this.p1.play("idle");
 			}
 		});
 	}
 
-	update() {}
+	update() {
+		// Update the position of the dots to match p1's and p2's origin positions
+		// Update p1's origin dot
+		if (this.p1 && this.p1OriginDot) {
+			const p1OriginX = this.p1.x;
+			const p1OriginY = this.p1.y;
+			this.p1OriginDot.setPosition(p1OriginX, p1OriginY);
+		}
+		
+		// Update p2's origin dot
+		if (this.p2 && this.p2OriginDot) {
+			const p2OriginX = this.p2.x;
+			const p2OriginY = this.p2.y;
+			this.p2OriginDot.setPosition(p2OriginX, p2OriginY);
+		}
+	}
 }
